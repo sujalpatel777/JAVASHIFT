85f59d87d4fc30dcc70afd025e8722b7a8499706
sujalpatel777 <sujal191234@gmail.com>
2024-07-01 00:07:01 +0530

    add   2.datttype.js

---
+++ 2.dattype.js
@@ -0,0 +1,26 @@
+"use strick";  // treat all js code as newer version
+
+console.log(3+3); // 
+console.log(typeof"hitesh");
+
+
+let score=null
+
+
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+
+console.log(valueInNumber);
+//"33"=>33
+//"33abc"=>Nan
+//
+let isloggedIn=1
+// 1 is true
+//0 is false 
+let bolleanis=Boolean(isloggedIn)
+console.log(bolleanis);
+
+let someNUmber=33
+let strinNumbers=String(someNUmber)
+console.log(typeof strinNumbers);
---
+++ varible_1.js
@@ -0,0 +1,13 @@
+const accountId=1455
+let accountemail="sujal191234@gmail.com"
+var accountpassword="12345"
+accountcity="jaipur"
+
+accountemail="jamin191234@gmail.com"
+accountpassword="123453"
+accountcity="valsad"
+/*
+this is note not to be account not include
+*/
+console.log(accountId);
+console.table([accountemail,accountpassword,accountcity])
