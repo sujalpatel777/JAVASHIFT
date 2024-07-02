43d9b4be3dbfe2fe4b9cdd0df6c3dde67c20eedc
sujalpatel777 <sujal191234@gmail.com>
2024-07-02 20:46:59 +0530

    add some feature

--- date_and_time.js
+++ date_and_time.js
@@ -1,4 +1,14 @@
 //date
 
 let myDate=new Date()
-console.log(myDate.toString);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+let myCreatedDate=new Date(2023,0,23,5,3)
+console.log(myCreatedDate.toDateString());
+
+let myTimeStape=Date.now()
+
+console.log(myTimeStape);
+console.log(myCreatedDate.getTime());
